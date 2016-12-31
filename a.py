dc939c2a8af578b7e0d40d8550c4d4d097945a47
James Knighton <iamknighton@gmail.com>
2016-12-31 12:49:30 -0800

    Refactor.

--- a.py
+++ a.py
@@ -1,60 +1,59 @@
 """ Trains an agent with (stochastic) Policy Gradients on Pong. Uses OpenAI Gym. """
 
+from argparse import ArgumentParser
 import numpy as np
 import cPickle as pickle
 import gym
 
-# hyperparameters
-H = 200 # number of hidden layer neurons
-batch_size = 10 # every how many episodes to do a param update?
-learning_rate = 1e-4
-gamma = 0.99 # discount factor for reward
-decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
-resume = False # resume from previous checkpoint?
-render = False
 
-# model initialization
-D = 80 * 80 # input dimensionality: 80x80 grid
-if resume:
-    model = pickle.load(open('save.p', 'rb'))
-else:
-    model = {}
-    model['W1'] = np.random.randn(H,D) / np.sqrt(D) # "Xavier" initialization
-    model['W2'] = np.random.randn(H) / np.sqrt(H)
+def parse_args():
+    ap = ArgumentParser()
+    ap.add_argument('--nb_hidden_neurons', type=int, default=200)
+    ap.add_argument('--batch_size', type=int, default=10)
+    ap.add_argument('--learning_rate', type=int, default=1e-4)
+    ap.add_argument('--gama', type=float, default=0.99)  # discount factor for reward
+    ap.add_argument('--decay_rate', type=float, default=0.99)  # decay factor for RMSProp leaky sum of grad^2
+    ap.add_argument('--resume', type=int, default=0)  # resume from previous checkpoint?
+    ap.add_argument('--render', type=int, default=True)
+    ap.add_argument('--input_dim', type=int, default=80 * 80)  # input dimensionality: 80x80 grid
+    return ap.parse_args()
 
-grad_buffer = { k : np.zeros_like(v) for k,v in model.iteritems() } # update buffers that add up gradients over a batch
-rmsprop_cache = { k : np.zeros_like(v) for k,v in model.iteritems() } # rmsprop memory
 
 def sigmoid(x):
-    return 1.0 / (1.0 + np.exp(-x)) # sigmoid "squashing" function to interval [0,1]
+    return 1.0 / (1.0 + np.exp(-x))
 
-def prepro(I):
-    """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
-    I = I[35:195] # crop
-    I = I[::2,::2,0] # downsample by factor of 2
-    I[I == 144] = 0 # erase background (background type 1)
-    I[I == 109] = 0 # erase background (background type 2)
-    I[I != 0] = 1 # everything else (paddles, ball) just set to 1
+
+def preprocess(I):
+    """ preprocess 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
+    I = I[35:195]     # crop
+    I = I[::2,::2,0]  # downsample by factor of 2
+    I[I == 144] = 0   # erase background (background type 1)
+    I[I == 109] = 0   # erase background (background type 2)
+    I[I != 0] = 1     # everything else (paddles, ball) just set to 1
     return I.astype(np.float).ravel()
 
-def discount_rewards(r):
+
+def discount_rewards(r, gamma):
     """ take 1D float array of rewards and compute discounted reward """
     discounted_r = np.zeros_like(r)
     running_add = 0
     for t in reversed(xrange(0, r.size)):
-        if r[t] != 0: running_add = 0 # reset the sum, since this was a game boundary (pong specific!)
+        if r[t] != 0:
+            running_add = 0  # reset the sum, since this was a game boundary (pong specific!)
         running_add = running_add * gamma + r[t]
         discounted_r[t] = running_add
     return discounted_r
 
-def policy_forward(x):
+
+def policy_forward(model, x):
     h = np.dot(model['W1'], x)
     h[h<0] = 0 # ReLU nonlinearity
     logp = np.dot(model['W2'], h)
     p = sigmoid(logp)
     return p, h # return probability of taking action 2, and hidden state
 
-def policy_backward(eph, epdlogp):
+
+def policy_backward(model,eph, epdlogp):
     """ backward pass. (eph is array of intermediate hidden states) """
     dW2 = np.dot(eph.T, epdlogp).ravel()
     dh = np.outer(epdlogp, model['W2'])
@@ -62,6 +61,17 @@ def policy_backward(eph, epdlogp):
     dW1 = np.dot(dh.T, epx)
     return {'W1':dW1, 'W2':dW2}
 
+
+args = parse_args()
+if args.resume:
+    model = pickle.load(open('save.p', 'rb'))
+else:
+    model = {}
+    model['W1'] = np.random.randn(args.nb_hidden_neurons, args.input_dim) / np.sqrt(args.input_dim) # "Xavier" initialization
+    model['W2'] = np.random.randn(args.nb_hidden_neurons) / np.sqrt(args.nb_hidden_neurons)
+
+grad_buffer = { k : np.zeros_like(v) for k,v in model.iteritems() } # update buffers that add up gradients over a batch
+rmsprop_cache = { k : np.zeros_like(v) for k,v in model.iteritems() } # rmsprop memory
 env = gym.make("Pong-v0")
 observation = env.reset()
 prev_x = None # used in computing the difference frame
@@ -70,15 +80,16 @@ running_reward = None
 reward_sum = 0
 episode_number = 0
 while True:
-    if render: env.render()
+    if args.render:
+        env.render()
 
     # preprocess the observation, set input to network to be difference image
-    cur_x = prepro(observation)
-    x = cur_x - prev_x if prev_x is not None else np.zeros(D)
+    cur_x = preprocess(observation)
+    x = cur_x - prev_x if prev_x is not None else np.zeros(args.input_dim)
     prev_x = cur_x
 
     # forward the policy network and sample an action from the returned probability
-    aprob, h = policy_forward(x)
+    aprob, h = policy_forward(model, x)
     action = 2 if np.random.uniform() < aprob else 3 # roll the dice!
 
     # record various intermediates (needed later for backprop)
@@ -104,21 +115,21 @@ while True:
         xs,hs,dlogps,drs = [],[],[],[] # reset array memory
 
         # compute the discounted reward backwards through time
-        discounted_epr = discount_rewards(epr)
+        discounted_epr = discount_rewards(epr, args.gamma)
         # standardize the rewards to be unit normal (helps control the gradient estimator variance)
         discounted_epr -= np.mean(discounted_epr)
         discounted_epr /= np.std(discounted_epr)
 
         epdlogp *= discounted_epr # modulate the gradient with advantage (PG magic happens right here.)
-        grad = policy_backward(eph, epdlogp)
+        grad = policy_backward(model, eph, epdlogp)
         for k in model: grad_buffer[k] += grad[k] # accumulate grad over batch
 
         # perform rmsprop parameter update every batch_size episodes
-        if episode_number % batch_size == 0:
+        if episode_number % args.batch_size == 0:
             for k,v in model.iteritems():
                 g = grad_buffer[k] # gradient
-                rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g**2
-                model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
+                rmsprop_cache[k] = args.decay_rate * rmsprop_cache[k] + (1 - args.decay_rate) * g**2
+                model[k] += args.learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
                 grad_buffer[k] = np.zeros_like(v) # reset batch gradient buffer
 
         # boring book-keeping
